f26a0f728c54677302f5ddeb5e786aa6120205b1
1sh4sk <ayeshask.24a.mailme@gmail.com>
2025-03-31 22:12:42 +0530

    responsiveness

--- frontend/src/pages/captain/CaptainHomeLayout.jsx
+++ frontend/src/pages/captain/CaptainHomeLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import CaptainHeader from "../../components/captain/captainHeader";
+import CaptainHeader from "../../components/captain/CaptainHeader";
 import { LiveTracking } from "../../components";
 
 const CaptainHomeLayout = () => {
