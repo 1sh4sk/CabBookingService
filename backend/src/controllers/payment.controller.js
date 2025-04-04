68cf64bea7429aa55b574a370210ddb40fc78672
Vishnuu16 <vvarthan351@gmail.com>
2025-04-04 10:46:09 +0530

    error earning

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -2,7 +2,9 @@ const { instance } = require('../../src/payment');
 
 const crypto = require('crypto');
 const paymentModel = require('../models/payment.model');
-
+const captainModel = require('../models/captain.model');
+const adminModel = require('../models/admin.model');
+const { sendMessageToSocketId } = require('../socket');
 
 
 
@@ -27,10 +29,212 @@ const checkouts = async (req, res) => {
 };
 
 
+// const paymentVerification = async (req, res, io) => {
+//     try {
+
+//         const { razorpay_order_id, razorpay_payment_id, razorpay_signature, captainId, fare } = req.body
+
+//         if (!fare || !captainId) {
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "Fare and Captain ID are required"
+//             });
+//         }
+//         //Create a string using the order ID and payment ID
+//         const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+//         //  Generate HMAC (Hash-based Message Authentication Code) using 'crypto' module
+//         const expectedSignature = crypto
+//             .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+//             .update(body)
+//             .digest('hex');
+
+//         if (expectedSignature === razorpay_signature) {
+           
+//             const commission = fare * 0.7;
+//             const companyEarnings = fare * 0.3;
+
+//             // Find the captain and update earnings
+//             const captain = await captainModel.findByIdAndUpdate(
+//                 captainId,
+//                 { $inc: { earnings: commission } }, // Increment earnings
+//                 { new: true }
+//             );
+
+//             if (!captain) {
+//                 return res.status(404).json({ success: false, message: "Captain not found" });
+//             }
+
+//             // Update admin/company earnings (assuming there's only one admin record)
+//             const admin = await adminModel.findOneAndUpdate(
+//                 {}, // Update the first found admin (you may need a specific admin ID)
+//                 { $inc: { earnings: companyEarnings } }, // Increment admin earnings
+//                 { new: true, upsert: true } // Create if not found
+//             );
+
+//             // Emit socket message to the captain
+//             io.to(captain.socketId).emit("payment_received", {
+//                 message: `You have received Ride Earnings: ₹${commission}`,
+//                 earnings: captain.earnings
+//             });
+
+
+//             res.status(200).json({
+//                 success: true,
+//                 message: "Payment verified and captain updated",
+//                 commission,
+//                 companyEarnings
+//             });
+//         }
+//         else {
+//             res.status(400).json({ success: false, message: "Payment verification failed" });
+//         }
+//         // console.log("sig received ", razorpay_signature);
+//         // console.log("sig generated ", expectedSignature);
+
+//         //Compare the generated signature with the one received from Razorpay
+//         // const verifysign = expectedSignature === razorpay_signature;
+
+
+//         // console.log("verifysign", verifysign);
+
+//         // if (verifysign) {
+//         //     await paymentModel.create({
+//         //         razorpay_order_id,
+//         //         razorpay_payment_id,
+//         //         razorpay_signature
+//         //     })
+
+//         //     res.redirect(
+//         //         `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
+//         //     );
+//         // } else {
+
+//         //     res.status(400).json({
+//         //         success: false,
+//         //         message: 'Payment verification failed verifiysign is false',
+//         //     });
+//         // }
+
+
+//     } catch (error) {
+//         console.error('Error creating order:', error);
+//         res.status(500).json({ success: false, message: 'Order creation failed', error });
+//     }
+
+// };
+
+
+
+
+// const paymentVerification = async (req, res, io) => {
+//     try {
+
+//         const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body
+//         const { captainId, fare } = req.query; // Extracting captainId and fare from the query parameters
+
+//         if (!fare || !captainId) {
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "Fare and Captain ID are required"
+//             });
+//         }
+//         //Create a string using the order ID and payment ID
+//         const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+//         //  Generate HMAC (Hash-based Message Authentication Code) using 'crypto' module
+//         const expectedSignature = crypto
+//             .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+//             .update(body)
+//             .digest('hex');
+
+//         if (expectedSignature === razorpay_signature) {
+
+//             const commission = Math.round(fare * 0.7);
+//             const companyEarnings = Math.round(fare * 0.3);
+
+//             // Find the captain and update earnings
+//             const captain = await captainModel.findByIdAndUpdate(
+//                 captainId,
+//                 { $inc: { earnings: commission } }, // Increment earnings
+//                 { new: true }
+//             );
+
+//             if (!captain) {
+//                 return res.status(404).json({ success: false, message: "Captain not found" });
+//             }
+
+//             // Update admin/company earnings (assuming there's only one admin record)
+//             const admin = await adminModel.findOneAndUpdate(
+//                 {}, // Update the first found admin (you may need a specific admin ID)
+//                 { $inc: { earnings: companyEarnings } }, // Increment admin earnings
+//                 { new: true, upsert: true } // Create if not found
+//             );
+
+//             // Emit socket message to the captain
+//             sendMessageToSocketId(captain.socketId, {
+//                 event: "payment-received",
+//                 data: {
+//                     message: "Payment received",
+//                     fare,
+//                     earnings: commission
+//                 }
+//             })
+
+          
+//             res.status(200).json({
+//                 success: true,
+//                 message: "Payment verified and captain updated",
+//                 commission,
+//                 companyEarnings
+//             });
+//             res.redirect(
+//                 ` http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}  `     
+//              );
+           
+//         }
+//         else {
+//             res.status(400).json({ success: false, message: "Payment verification failed" });
+//         }
+//         // console.log("sig received ", razorpay_signature);
+//         // console.log("sig generated ", expectedSignature);
+
+//         //Compare the generated signature with the one received from Razorpay
+//         // const verifysign = expectedSignature === razorpay_signature;
+
+
+//         // console.log("verifysign", verifysign);
+
+//         // if (verifysign) {
+//         //     await paymentModel.create({
+//         //         razorpay_order_id,
+//         //         razorpay_payment_id,
+//         //         razorpay_signature
+//         //     })
+
+//         //     res.redirect(
+//         //         http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}
+//         //     );
+//         // } else {
+
+//         //     res.status(400).json({
+//         //         success: false,
+//         //         message: 'Payment verification failed verifiysign is false',
+//         //     });
+//         // }
+
+
+//     } catch (error) {
+//         console.error('Error creating order:', error);
+//         res.status(500).json({ success: false, message: 'Order creation failed', error });
+//     }
+
+// };
+
 const paymentVerification = async (req, res, io) => {
     try {
-
-        const { razorpay_order_id, razorpay_payment_id, razorpay_signature, captainId, fare } = req.body
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+        const { captainId, fare } = req.query;
 
         if (!fare || !captainId) {
             return res.status(400).json({
@@ -38,90 +242,51 @@ const paymentVerification = async (req, res, io) => {
                 message: "Fare and Captain ID are required"
             });
         }
-        //Create a string using the order ID and payment ID
-        const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-        //  Generate HMAC (Hash-based Message Authentication Code) using 'crypto' module
+        const body = `${razorpay_order_id}|${razorpay_payment_id}`;
         const expectedSignature = crypto
             .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
             .update(body)
             .digest('hex');
 
-        if (expectedSignature === razorpay_signature) {
-           
-            const commission = fare * 0.7;
-            const companyEarnings = fare * 0.3;
+        if (expectedSignature !== razorpay_signature) {
+            return res.status(400).json({ success: false, message: "Payment verification failed" });
+        }
 
-            // Find the captain and update earnings
-            const captain = await captainModel.findByIdAndUpdate(
-                captainId,
-                { $inc: { earnings: commission } }, // Increment earnings
-                { new: true }
-            );
+        const commission = Math.round(fare * 0.7);
+        const companyEarnings = Math.round(fare * 0.3);
 
-            if (!captain) {
-                return res.status(404).json({ success: false, message: "Captain not found" });
+        const captain = await captainModel.findByIdAndUpdate(
+            captainId,
+            { $inc: { earnings: commission } },
+            { new: true }
+        );
+
+        if (!captain) {
+            return res.status(404).json({ success: false, message: "Captain not found" });
+        }
+
+        await adminModel.findOneAndUpdate(
+            {},
+            { $inc: { earnings: companyEarnings } },
+            { new: true, upsert: true }
+        );
+
+        sendMessageToSocketId(captain.socketId, {
+            event: "payment-received",
+            data: {
+                message: "Payment received",
+                fare,
+                earnings: commission
             }
+        });
 
-            // Update admin/company earnings (assuming there's only one admin record)
-            const admin = await adminModel.findOneAndUpdate(
-                {}, // Update the first found admin (you may need a specific admin ID)
-                { $inc: { earnings: companyEarnings } }, // Increment admin earnings
-                { new: true, upsert: true } // Create if not found
-            );
-
-            // Emit socket message to the captain
-            io.to(captain.socketId).emit("payment_received", {
-                message: `You have received Ride Earnings: ₹${commission}`,
-                earnings: captain.earnings
-            });
-
-
-            res.status(200).json({
-                success: true,
-                message: "Payment verified and captain updated",
-                commission,
-                companyEarnings
-            });
-        }
-        else {
-            res.status(400).json({ success: false, message: "Payment verification failed" });
-        }
-        // console.log("sig received ", razorpay_signature);
-        // console.log("sig generated ", expectedSignature);
-
-        //Compare the generated signature with the one received from Razorpay
-        // const verifysign = expectedSignature === razorpay_signature;
-
-
-        // console.log("verifysign", verifysign);
-
-        // if (verifysign) {
-        //     await paymentModel.create({
-        //         razorpay_order_id,
-        //         razorpay_payment_id,
-        //         razorpay_signature
-        //     })
-
-        //     res.redirect(
-        //         `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
-        //     );
-        // } else {
-
-        //     res.status(400).json({
-        //         success: false,
-        //         message: 'Payment verification failed verifiysign is false',
-        //     });
-        // }
-
-
+        return res.redirect(`http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`);
+        
     } catch (error) {
-        console.error('Error creating order:', error);
-        res.status(500).json({ success: false, message: 'Order creation failed', error });
+        console.error('Error verifying payment:', error);
+        return res.status(500).json({ success: false, message: 'Payment verification failed', error });
     }
-
 };
-
-
 
 module.exports = { checkouts, paymentVerification };
--- frontend/src/pages/user/MakePayment.jsx
+++ frontend/src/pages/user/MakePayment.jsx
@@ -1,3 +1,123 @@
+// import { faLocationArrow, faMapMarkerAlt, faMapPin, faPhoneVolume, faShieldAlt, faWallet } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { useContext } from "react";
+// import { useLocation, useNavigate } from "react-router";
+// import { SocketContext } from "../../context/SocketContext";
+// import { checkoutApi, getKey } from "../../api/paymentApi";
+// import { userDataContext } from "../../context/UserContext";
+
+// const MakePayment = () => {
+
+//     const location = useLocation();
+//     const rideDetail = location.state || {};
+//     // const { receiveMessage } = useContext(SocketContext);
+//     const { user } = useContext(userDataContext);
+//     const navigate = useNavigate();
+
+//     // receiveMessage('ride-ended', ride => {
+//     //     navigate('/home')
+//     // })
+
+//     console.log(user);
+
+
+
+//     const handlePaymentCheckout = async (amount) => {
+//         const { data: { key } } = await getKey();
+
+
+//         const res = await checkoutApi(amount);
+
+//         const { order } = res.data;
+//         // Open Razorpay Checkout
+//         const options = {
+//             key, // Replace with your Razorpay key_id
+//             amount: order.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+//             currency: 'INR',
+//             name: 'user',
+//             description: 'Test Transaction',
+//             order_id: order.id, // This is the order_id created in the backend
+//             callback_url: 'http://localhost:7000/payment/paymentverification', // Your success URL
+//             prefill: {
+//                 name: 'Gaurav Kumar',
+//                 email: 'gaurav.kumar@example.com',
+//                 contact: '9999999999'
+//             },
+//             theme: {
+//                 color: '#F7B401'
+//             },
+//         };
+
+//         const razor = new window.Razorpay(options);
+//         razor.open()
+//     }
+
+//     return <div className='w-full lg:w-1/2 flex flex-col p-4 bg-white rounded-3xl lg:rounded-none -mt-14 lg:mt-0 z-10'>
+//         {/* Driver Info */}
+//         <div className="w-full flex items-center gap-4 mb-4">
+//             <img src="https://images.pexels.com/photos/5835588/pexels-photo-5835588.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Driver" className="w-14 h-14 rounded-full object-cover" />
+//             <div className='w-[90%] lg:w-full flex items-center justify-between'>
+//                 <div>
+//                     <h6 className="font-bold text-base capitalize">{rideDetail?.captain?.fullname?.firstname} {rideDetail?.captain?.fullname?.lastname}</h6>
+//                 </div>
+//                 <div className='text-right'>
+//                     <h2 className="text-lg font-bold">{rideDetail?.captain?.vehicle?.plate}</h2>
+//                     <p className="text-gray-500 text-sm">{rideDetail?.captain?.vehicle?.vehiclename}</p>
+//                     <p className="text-[#F7B401] font-bold text-sm">⭐ 4.5</p>
+//                 </div>
+//             </div>
+//         </div>
+
+//         {/* Message Input */}
+//         <div className="flex items-center border border-gray-200 rounded bg-gray-100 p-2">
+//             <input type="text" placeholder="Send a message to the driver" className="w-full bg-gray-100 focus:outline-none text-base m-0!" />
+//             <img src='/src/assets/Map-Arrow-Right--Streamline-Solar.svg' className='h-5 w-5' alt="Send" />
+//         </div>
+
+//         {/* Action Buttons */}
+//         <div className='flex justify-around mt-4'>
+//             {[{ icon: faShieldAlt, label: "Safety" }, { icon: faLocationArrow, label: "Location" }, { icon: faPhoneVolume, label: "Call" }].map(({ icon, label }) => (
+//                 <button key={label} className="flex flex-col items-center">
+//                     <div className="bg-gray-100 p-4 rounded-full">
+//                         <FontAwesomeIcon icon={icon} className="text-[#F7B401] text-2xl" />
+//                     </div>
+//                     <span className="text-sm font-bold mt-1">{label}</span>
+//                 </button>
+//             ))}
+//         </div>
+
+
+//         <div className="mt-10 space-y-3">
+//             <div className='flex items-center gap-4 border-b pb-2'>
+//                 <FontAwesomeIcon icon={faMapMarkerAlt} className="text-[#F7B401] text-2xl" />
+//                 <h1 className='font-medium text-base'>
+//                     {rideDetail?.pickup}
+//                 </h1>
+//             </div>
+//             <div className='flex items-center gap-4 border-b pb-2'>
+//                 <FontAwesomeIcon icon={faMapPin} className="text-[#F7B401] text-2xl" />
+//                 <h1 className='font-medium text-base'>
+//                     {rideDetail?.destination}
+//                 </h1>
+//             </div>
+//             <div className='flex items-center gap-4 pb-2'>
+//                 <FontAwesomeIcon icon={faWallet} className="text-[#F7B401] text-2xl" />
+//                 <h1 className='font-medium text-base'>
+//                     ₹{rideDetail?.fare}
+//                 </h1>
+//             </div>
+//         </div>
+
+//         {/* Payment Button */}
+//         <button onClick={() => handlePaymentCheckout(rideDetail?.fare)} className='w-full p-3 mt-6 rounded-xl bg-green-500 text-white font-bold text-base'>
+//             Make a Payment
+//         </button>
+//     </div>;
+// };
+// export default MakePayment;
+
+
+
 import { faLocationArrow, faMapMarkerAlt, faMapPin, faPhoneVolume, faShieldAlt, faWallet } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
@@ -37,7 +157,7 @@ const MakePayment = () => {
             name: 'user',
             description: 'Test Transaction',
             order_id: order.id, // This is the order_id created in the backend
-            callback_url: 'http://localhost:7000/payment/paymentverification', // Your success URL
+            callback_url:` http://localhost:7000/payment/paymentverification?captainId=${rideDetail?.captain?._id}&fare=${order.amount}`, // Your success URL
             prefill: {
                 name: 'Gaurav Kumar',
                 email: 'gaurav.kumar@example.com',
@@ -47,6 +167,7 @@ const MakePayment = () => {
                 color: '#F7B401'
             },
         };
+
 
         const razor = new window.Razorpay(options);
         razor.open()
@@ -90,23 +211,23 @@ const MakePayment = () => {
         <div className="mt-10 space-y-3">
             <div className='flex items-center gap-4 border-b pb-2'>
                 <FontAwesomeIcon icon={faMapMarkerAlt} className="text-[#F7B401] text-2xl" />
-                <h1 className='font-medium text-base'>
+                <h1 className='font-medium text-base lg:font-sm xl:font-base'>
                     {rideDetail?.pickup}
                 </h1>
             </div>
             <div className='flex items-center gap-4 border-b pb-2'>
                 <FontAwesomeIcon icon={faMapPin} className="text-[#F7B401] text-2xl" />
-                <h1 className='font-medium text-base'>
+                <h1 className='font-medium text-base lg:font-sm xl:font-base'>
                     {rideDetail?.destination}
                 </h1>
             </div>
             <div className='flex items-center gap-4 pb-2'>
                 <FontAwesomeIcon icon={faWallet} className="text-[#F7B401] text-2xl" />
-                <h1 className='font-medium text-base'>
+                <h1 className='font-medium text-base lg:font-sm xl:font-base'>
                     ₹{rideDetail?.fare}
                 </h1>
             </div>
-        </div>
+        </div> 
 
         {/* Payment Button */}
         <button onClick={() => handlePaymentCheckout(rideDetail?.fare)} className='w-full p-3 mt-6 rounded-xl bg-green-500 text-white font-bold text-base'>
